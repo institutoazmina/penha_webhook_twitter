1925be0c5c2248cfa1ddb985ca6064e1431879a9
Lucas Ansei <lucas.ansei@appcivico.com>
2021-06-23 14:48:25 -0300

    current dev state

--- src/api/routes/twitter-webhook.js
+++ src/api/routes/twitter-webhook.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
+const redis = require('redis');
+
+const redis_client = redis.createClient();
+console.log(redis_client);
 
 function get_challenge_response(crc_token, consumer_secret) {
     return crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64');
@@ -24,7 +28,7 @@ router.post('/twitter-webhook', (req, res) => {
 
     direct_messages.forEach(dm => {
         const twitter_user_id = dm.message_create.sender_id;
-        const remote_id = createHmac('sha256', twitter_user_id).digest('hex');
+        const remote_id = crypto.createHmac('sha256', twitter_user_id).digest('hex');
     });
 
     return res.json({ message: 'ok' });
