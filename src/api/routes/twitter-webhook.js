5f1eced02567bc54f0dd03c8fdb81e9b3631926c
Lucas Ansei <lucas.ansei@appcivico.com>
2021-06-24 11:44:15 -0300

    Adding async

--- src/api/routes/twitter-webhook.js
+++ src/api/routes/twitter-webhook.js
@@ -23,6 +23,25 @@ function get_challenge_response(crc_token, consumer_secret) {
     return crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64');
 };
 
+async function post_questionnaire(twitter_user_id, questionnaire_id) {
+    const bodyFormData = new FormData();
+    bodyFormData.append('token', process.env.PENHAS_API_TOKEN);
+    bodyFormData.append('remote_id', twitter_user_id);
+    bodyFormData.append('questionnaire_id', questionnaire_id);
+    console.log('fazendo post do questionario\n')
+
+    return axios({
+        method: 'post',
+        url: 'https://dev-penhas-api.appcivico.com/anon-questionnaires/new',
+        data: bodyFormData,
+        headers: bodyFormData.getHeaders(),
+    });
+}
+
+async function get_stash(twitter_user_id) {
+    return redis_client.get(twitter_user_id);
+}
+
 router.get('/twitter-webhook', (req, res) => {
     const { crc_token } = req.query;
 
@@ -36,141 +55,139 @@ router.get('/twitter-webhook', (req, res) => {
     return res.json({ response_token: 'sha256=' + get_challenge_response(crc_token, consumer_secret) });
 });
 
-router.post('/twitter-webhook', (req, res) => {
+router.post('/twitter-webhook', async (req, res) => {
     const direct_messages = req.body.direct_message_events;
 
-    direct_messages.forEach(dm => {
+    direct_messages.forEach(async dm => {
         const twitter_user_id = dm.message_create.sender_id;
         const remote_id = crypto.createHmac('sha256', twitter_user_id).digest('hex');
 
-        console.log('twitter_user_id :' + twitter_user_id)
-        const foo = redis_client.get(twitter_user_id, (err, reply) => {
+        stash = await get_stash(twitter_user_id);
+        console.log(stash);
+        //         console.log('twitter_user_id :' + twitter_user_id)
+        //         const foo = redis_client.get(twitter_user_id, (err, reply) => {
 
-            if (reply) {
-                console.log('stash encontrada\n')
-                let stash = JSON.parse(reply);
-                console.log(stash)
+        //             if (reply) {
+        //                 console.log('stash encontrada\n')
+        //                 let stash = JSON.parse(reply);
+        //                 console.log(stash)
 
-                let node = flow.filter((n) => {
-                    return n.code === stash.current_node;
-                });
-                node = node[0];
+        //                 let node = flow.filter((n) => {
+        //                     return n.code === stash.current_node;
+        //                 });
+        //                 node = node[0];
 
-                if (dm.message_create.message_data.quick_reply_response) {
-                    const quick_reply = dm.message_create.message_data.quick_reply_response.metadata;
+        //                 if (dm.message_create.message_data.quick_reply_response) {
+        //                     const quick_reply = dm.message_create.message_data.quick_reply_response.metadata;
 
-                    if (quick_reply.substring(0, 13) === 'questionnaire') {
-                        console.log('QR foi uma resposta de questionario\n')
-                        const chosen_opt = quick_reply.substring(14);
-                        console.log('chosen_opt: ' + chosen_opt)
-                    }
-                    else {
-                        console.log('QR foi um botão do fluxo\n')
+        //                     if (quick_reply.substring(0, 13) === 'questionnaire') {
+        //                         console.log('QR foi uma resposta de questionario\n')
+        //                         const chosen_opt = quick_reply.substring(14);
+        //                         console.log('chosen_opt: ' + chosen_opt)
+        //                     }
+        //                     else {
+        //                         console.log('QR foi um botão do fluxo\n')
 
-                        let next_node = flow.filter((n) => {
-                            return n.code === quick_reply;
-                        });
-                        next_node = next_node[0];
+        //                         let next_node = flow.filter((n) => {
+        //                             return n.code === quick_reply;
+        //                         });
+        //                         next_node = next_node[0];
 
-                        if (next_node.questionnaire_id) {
-                            const bodyFormData = new FormData();
-                            bodyFormData.append('token', process.env.PENHAS_API_TOKEN);
-                            bodyFormData.append('remote_id', twitter_user_id);
-                            bodyFormData.append('questionnaire_id', next_node.questionnaire_id);
+        //                         if (next_node.questionnaire_id) {
 
-                            console.log('fazendo post do questionario\n')
-                            axios({
-                                method: 'post',
-                                url: 'https://dev-penhas-api.appcivico.com/anon-questionnaires/new',
-                                data: bodyFormData,
-                                headers: bodyFormData.getHeaders(),
-                            }).then((res) => {
-                                const next_message = res.data.quiz_session.current_msg;
+        //                             await post_questionnaire(twitter_user_id, next_node, questionnaire_id);
+        //                             // axios({
+        //                             //     method: 'post',
+        //                             //     url: 'https://dev-penhas-api.appcivico.com/anon-questionnaires/new',
+        //                             //     data: bodyFormData,
+        //                             //     headers: bodyFormData.getHeaders(),
+        //                             // }).then((res) => {
+        //                             //     const next_message = res.data.quiz_session.current_msg;
 
-                                if (next_message) {
-                                    console.log('fazendo post da proxima mensagem\n')
-                                    client.post("direct_messages/events/new", {
-                                        event: {
-                                            type: "message_create",
+        //                             //     if (next_message) {
+        //                             //         console.log('fazendo post da proxima mensagem\n')
+        //                             //         client.post("direct_messages/events/new", {
+        //                             //             event: {
+        //                             //                 type: "message_create",
 
-                                            message_create: {
+        //                             //                 message_create: {
 
-                                                target: { recipient_id: twitter_user_id },
+        //                             //                     target: { recipient_id: twitter_user_id },
 
-                                                message_data: {
-                                                    text: next_message.content,
-                                                    quick_reply: {
-                                                        type: 'options',
-                                                        options: next_message.options.map((opt) => {
-                                                            return { label: opt.display.substring(0, 36), metadata: 'questionnaire_' + opt.code_value }
-                                                        })
-                                                    }
-                                                },
+        //                             //                     message_data: {
+        //                             //                         text: next_message.content,
+        //                             //                         quick_reply: {
+        //                             //                             type: 'options',
+        //                             //                             options: next_message.options.map((opt) => {
+        //                             //                                 return { label: opt.display.substring(0, 36), metadata: 'questionnaire_' + opt.code_value }
+        //                             //                             })
+        //                             //                         }
+        //                             //                     },
 
-                                            }
-                                        }
-                                    }).catch(err => {
-                                        console.log('erro no post dm/events/new\n');
-                                        console.log(err);
-                                    })
+        //                             //                 }
+        //                             //             }
+        //                             //         }).catch(err => {
+        //                             //             console.log('erro no post dm/events/new\n');
+        //                             //             console.log(err);
+        //                             //         })
 
-                                    stash.current_node = next_node.code;
-                                    stash.is_questionnaire = true;
-                                    stash.current_questionnaire_question = next_message.code
-                                    redis_client.set(twitter_user_id, JSON.stringify(stash));
+        //                             // stash.current_node = next_node.code;
+        //                             // stash.is_questionnaire = true;
+        //                             // stash.current_questionnaire_question = next_message.code
+        //                             // redis_client.set(twitter_user_id, JSON.stringify(stash));
 
-                                }
+        //                         }
 
-                            }).catch((err) => {
-                                console.log('erro no post anon-questionnaires/new\n');
-                                console.log(err)
-                            })
-                        }
+        //                     }).catch((err) => {
+        //                         console.log('erro no post anon-questionnaires/new\n');
+        //                         console.log(err)
+        //                     })
+        //     }
 
-                        console.log(next_node);
-                    }
-                }
-            }
-            else {
-                // Começando coversa
-                const node = flow[0];
-                const step = {
-                    current_node: flow[0].code,
-                    started_at: Date.now()
-                }
-                redis_client.set(twitter_user_id, JSON.stringify(step));
-                console.log(node.quick_replies);
-                // Verificando por mensagens
-                const messages = node.messages;
-                if (messages) {
+        //                         console.log(next_node);
+        // }
+        //                 }
+        //             }
+        //             else {
+        //         // Começando coversa
+        //         const node = flow[0];
+        //         const step = {
+        //             current_node: flow[0].code,
+        //             started_at: Date.now()
+        //         }
+        //                 redis_client.set(twitter_user_id, JSON.stringify(step));
+        //         console.log(node.quick_replies);
+        //         // Verificando por mensagens
+        //         const messages = node.messages;
+        //         if(messages) {
 
-                    client.post("direct_messages/events/new", {
-                        event: {
-                            type: "message_create",
+        //             client.post("direct_messages/events/new", {
+        //                 event: {
+        //                     type: "message_create",
 
-                            message_create: {
+        //                     message_create: {
 
-                                target: { recipient_id: twitter_user_id },
+        //                         target: { recipient_id: twitter_user_id },
 
-                                message_data: {
-                                    text: messages.join('\n'),
-                                    quick_reply: {
-                                        type: 'options',
-                                        options: node.quick_replies
-                                    }
-                                },
+        //                         message_data: {
+        //                             text: messages.join('\n'),
+        //                             quick_reply: {
+        //                                 type: 'options',
+        //                                 options: node.quick_replies
+        //                             }
+        //                         },
 
-                            }
-                        }
+        //                     }
+        //                 }
 
-                    }).catch(err => {
-                        console.log(err);
-                    })
-                }
+        //             }).catch(err => {
+        //                 console.log(err);
+        //             })
+        //         }
 
 
-            }
-        });
+        //     }
+        //         });
 
     });
 
