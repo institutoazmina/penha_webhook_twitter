f0f9b6484552da048989dd845f0f8bc63eaecf9b
Lucas Ansei <lucas.ansei@appcivico.com>
2021-06-24 12:56:36 -0300

    FIX: typo

--- src/api/routes/twitter-webhook.js
+++ src/api/routes/twitter-webhook.js
@@ -169,10 +169,10 @@ router.post('/twitter-webhook', async (req, res) => {
                     const answer = await post_answer(metadata.session_id, metadata.question_ref, metadata.index);
 
                     if (answer.data.quiz_session.current_msgs[0]) {
-                        const next_message = questionnaire_data.quiz_session.current_msgs[0];
+                        const next_message = answer.quiz_session.current_msgs[0];
 
                         await send_dm(twitter_user_id, next_message.content, next_message.options.map((opt) => {
-                            return { label: opt.display.substring(0, 36), metadata: JSON.stringify({ question_ref: next_message.ref, index: opt.index, session_id: questionnaire_data.quiz_session.session_id, is_questionnaire: true }) }
+                            return { label: opt.display.substring(0, 36), metadata: JSON.stringify({ question_ref: next_message.ref, index: opt.index, session_id: answer.quiz_session.session_id, is_questionnaire: true }) }
                         }));
 
                         stash.current_node = next_node.code;
