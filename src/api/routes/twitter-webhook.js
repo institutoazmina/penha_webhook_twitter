f9426104ad80ba25de2a1384c2dcf51b058754a8
Lucas Ansei <lucas.ansei@appcivico.com>
2021-07-30 16:31:10 -0300

    FIX: typo

--- src/api/routes/twitter-webhook.js
+++ src/api/routes/twitter-webhook.js
@@ -702,10 +702,9 @@ router.post('/twitter-webhook', async (req, res) => {
                                             else if (msg.code.substring(0, 3) === 'FIM') { }
                                             else {
 
-                                                const analytics_post = await analytics_api.post_analytics(stash.conversa_id, msg.code, stash.current_questionnaire_question, stash.first_msg_tz, 1, (stash.tag_code || await get_tag_code(metadata.code_value, flow.tag_code_config, twitter_user_id)), 'DURING_QUESTIONNAIRE', stash.current_questionnaire_id);
+                                                const analytics_post = await analytics_api.post_analytics(stash.conversa_id, msg.code, stash.current_questionnaire_question, stash.first_msg_tz, 1, (stash.tag_code || await get_tag_code(msg.code, flow.tag_code_config, twitter_user_id)), 'DURING_QUESTIONNAIRE', stash.current_questionnaire_id);
                                                 analytics_id = analytics_post.data.id;
 
-                                                stash.tag_code = await get_tag_code(metadata.code_value, flow.tag_code_config, twitter_user_id);
                                                 stash.last_analytics_id = analytics_id;
                                                 stash.current_questionnaire_question = msg.code;
                                                 stash.current_questionnaire_question_type = msg.type;
@@ -714,7 +713,6 @@ router.post('/twitter-webhook', async (req, res) => {
 
                                                 await stasher.save_stash(twitter_user_id, stash);
                                             }
-
                                         }
                                     },
                                     timeout
