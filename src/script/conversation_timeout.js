b13ccd0260087e8538b2c75d0354e2ea72cd8de8
Lucas Ansei <lucas.ansei@appcivico.com>
2021-08-02 17:50:17 -0300

    removing logs

--- src/script/conversation_timeout.js
+++ src/script/conversation_timeout.js
@@ -9,11 +9,9 @@ async function process_queue() {
     const ttl = 3600000;
 
     const lock = await redis.setnx('timeout_started_at', (Date.now() / 1000));
-    console.log(lock);
     if (lock === 1) {
         await redis.pexpire('timeout_started_at', ttl);
 
-        console.info('Buscando keys no redis');
         const keys = await redis.keys('*');
 
         let json_config = await redis.get('json_config');
@@ -27,7 +25,6 @@ async function process_queue() {
                 return;
             }
 
-            console.info('Buscando stash para twitter_id: ' + key);
             let stash = await redis.get(key);
             stash = JSON.parse(stash);
 
@@ -35,7 +32,6 @@ async function process_queue() {
             const seconds_since_last_msg = (Date.now() / 1000) - last_msg_epoch;
 
             if (seconds_since_last_msg >= config_timeout_seconds) {
-                console.info('É hora de dar timeout');
 
                 const twitter_user_id = key;
 
@@ -48,23 +44,16 @@ async function process_queue() {
                 return await redis.set(twitter_user_id, JSON.stringify(new_stash));
             }
             else {
-                console.info('ainda não é hora de dar timeout');
                 return 1;
             }
         });
 
-        console.info('Deletando key timeout_started_at no redis');
         await redis.del('timeout_started_at');
         process.exit(1);
-
-        return 1;
     }
     else {
-        console.info('Já há uma execução para o script de timeout');
         process.exit(1);
-        return 1;
     }
 }
 
 process_queue();
-
